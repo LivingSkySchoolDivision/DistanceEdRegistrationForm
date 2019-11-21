d7db1046503c2d6a4d34f986912da2a9e0baffc1
Mark Strendin <mark.strendin@lskysd.ca>
2019-11-20 22:33:22 -0600

    Add an asp.net core web frontend, and mock up an example site with a registration form.

--- LSSDDistanceEdReg/LSSDDistanceEdReg.Domain/DistanceEdClass.cs
+++ LSSDDistanceEdReg/LSSDDistanceEdReg.Domain/DistanceEdClass.cs
@@ -4,14 +4,19 @@ using System.Text;
 
 namespace LSSD.DistanceEdReg
 {
-    class DistanceEdClass
+    public class DistanceEdClass
     {
+        public int ID { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public DateTime AvailableFrom { get; set; }
         public DateTime AvailableTo { get; set; }
+        public string MoreInfoURL { get; set; }
 
         // Are the teaching materials available for teachers to request, without 
         // having students in a class?
         public bool MaterialsAvailableToTeachers { get; set; }
+
+        public bool MentorTeacherRequired { get; set; }
     }
 }
