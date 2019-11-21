1a609371a8757773bdf59e3048133a8b8221320f
Mark Strendin <mark.strendin@lskysd.ca>
2019-11-20 20:05:35 -0600

    Initial commit - start writing up some classes

---
+++ LSSDDistanceEdReg/LSSDDistanceEdReg.Domain/DistanceEdClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSSD.DistanceEdReg
+{
+    class DistanceEdClass
+    {
+        public string Name { get; set; }
+        public DateTime AvailableFrom { get; set; }
+        public DateTime AvailableTo { get; set; }
+
+        // Are the teaching materials available for teachers to request, without 
+        // having students in a class?
+        public bool MaterialsAvailableToTeachers { get; set; }
+    }
+}
---
+++ LSSDDistanceEdReg/LSSDDistanceEdReg.Domain/Registration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSSD.DistanceEdReg
+{
+    public class Registration
+    {
+        public string StudentName { get; set; }
+        public string StudentNumber { get; set; }
+        public string StudentSchool { get; set; }
+        public string Comments { get; set; }        
+    }
+}
