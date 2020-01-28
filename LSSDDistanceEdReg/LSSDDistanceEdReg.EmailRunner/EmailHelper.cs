d618ae5c597f0d7c4b66d1195da005029524fd93
Mark Strendin <mark.strendin@lskysd.ca>
2020-01-28 15:22:47 -0600

    System sends emails (to me, for now). We are currently blocked by a spam blackhole, so I'll need to sort that out first.

---
+++ LSSDDistanceEdReg/LSSDDistanceEdReg.EmailRunner/EmailHelper.cs
@@ -0,0 +1,95 @@
+﻿using LSSD.DistanceEdReg;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace LSSDDistanceEdReg.EmailRunner
+{
+
+    class EmailHelper
+    {
+        private string hostname;
+        private string username;
+        private string password;
+        private string fromaddress;
+        private string replyToAddress;
+        private int smtpPort;
+
+        public EmailHelper(string hostname, int smtpPort, string username, string password, string fromaddress, string replyToAddress)
+        {
+            this.hostname = hostname;
+            this.username = username;
+            this.password = password;
+            this.fromaddress = fromaddress;
+            this.smtpPort = smtpPort;
+            this.replyToAddress = replyToAddress;
+        }
+
+        Queue<DistanceEdRequest> _mailQueue = new Queue<DistanceEdRequest>();
+
+        public void NewMessage(DistanceEdRequest request)
+        {
+            this._mailQueue.Enqueue(request);
+        }
+
+        public void NewMessages(IEnumerable<DistanceEdRequest> requests)
+        {
+            foreach(DistanceEdRequest r in requests)
+            {
+                _mailQueue.Enqueue(r);
+            }
+        }
+               
+        /// <summary>
+        /// Sends all enqueued mail messages
+        /// </summary>
+        /// <returns>ID numbers of successful notifications</returns>
+        public List<int> SendAll()
+        {
+            List<int> successes = new List<int>();
+
+            if (this._mailQueue.Count > 0)
+            {
+                using (SmtpClient smtpClient = new SmtpClient(this.hostname)
+                {
+                    Port = this.smtpPort,
+                    UseDefaultCredentials = false,
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential(this.username, this.password)
+                })
+                {
+                    while (this._mailQueue.Count > 0)
+                    {
+                        DistanceEdRequest request = this._mailQueue.Dequeue();
+
+                        try
+                        {
+                            // Send the mail message and log the results in the DB
+                            MailMessage msg = new MailMessage();
+                            msg.To.Add("mark.strendin@lskysd.ca");
+                            msg.Body = "";
+                            msg.Subject = "Distance Ed Request for " + request.StudentName + " for class " + request.ID;
+                            msg.From = new MailAddress(this.fromaddress);
+                            msg.ReplyToList.Add(new MailAddress(this.replyToAddress));
+                            msg.IsBodyHtml = true;
+                            smtpClient.Send(msg);
+
+                            successes.Add(request.ID);
+                        }
+                        catch (Exception ex)
+                        {
+                            // Log a failure
+                            throw ex;
+                        }
+                    }
+
+                }
+            }
+
+            return successes;
+        }
+
+    }
+}
--- LSSDDistanceEdReg/LSSDDistanceEdReg.EmailRunner/Program.cs
+++ LSSDDistanceEdReg/LSSDDistanceEdReg.EmailRunner/Program.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.Azure.KeyVault;
+﻿using LSSD.DistanceEdReg;
+using LSSD.DistanceEdReg.Data;
+using LSSD.DistanceEdReg.Util;
+using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
 using System;
+using System.Collections.Generic;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace LSSDDistanceEdReg.EmailRunner
@@ -50,7 +55,31 @@ namespace LSSDDistanceEdReg.EmailRunner
                 ConsoleWrite("EmailRunner starting main loop...");
                 while (true)
                 {
+                    using (DistanceEdRequestRepository requestRepository = new DistanceEdRequestRepository(dbConnectionString))
+                    {
+                        List<DistanceEdRequest> requestsNeedingNotification = requestRepository.GetRequestsRequiringNotification();
 
+                        EmailHelper email = new EmailHelper(smtpConfig["hostname"], smtpConfig["port"].ToInt(), smtpConfig["username"], smtpConfig["password"], smtpConfig["fromaddress"], smtpConfig["replytoaddress"]);                        
+
+                        if (requestsNeedingNotification.Count > 0)
+                        {
+                            ConsoleWrite("Found " + requestsNeedingNotification.Count + " requests needing notifications");
+                            foreach(DistanceEdRequest r in requestsNeedingNotification )
+                            {
+                                ConsoleWrite("Enqueing notifcation for request ID " + r.ID);
+                                email.NewMessage(r);
+                            }                            
+                        } else
+                        {
+                            ConsoleWrite("No new requests found");
+                        }
+
+                        Console.WriteLine("Sending...");
+                        List<int> successfulIDs = email.SendAll();
+
+                        Console.WriteLine("Marking " + successfulIDs.Count + " as successes");
+                        requestRepository.MarkAsHelpDeskNotified(successfulIDs);
+                    }
 
                     ConsoleWrite("Done!");
                     ConsoleWrite("Sleeping for " + sleepTimeMinutes + " minutes...");
