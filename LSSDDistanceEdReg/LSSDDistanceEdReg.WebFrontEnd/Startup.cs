64ab81ba8e7db9ba4b380969ee02e1730dc58595
Mark Strendin <mark.strendin@lskysd.ca>
2020-02-05 13:18:14 -0600

    Enable developer error pages so I can troubleshoot this fucking oauth issue

--- LSSDDistanceEdReg/LSSDDistanceEdReg.WebFrontEnd/Startup.cs
+++ LSSDDistanceEdReg/LSSDDistanceEdReg.WebFrontEnd/Startup.cs
@@ -65,14 +65,16 @@ namespace LSSDDistanceEdReg.WebFrontEnd
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            /*
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+               app.UseDeveloperExceptionPage(); 
             }
             else
             {
                 app.UseExceptionHandler("/Error");
-            }
+            }*/
+            app.UseDeveloperExceptionPage();
 
             app.UseStaticFiles();
 
