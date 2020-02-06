893996a5e661f95b57bd6ecef86ffcd09f603d96
Mark Strendin <mark.strendin@gmail.com>
2020-02-05 20:19:52 -0600

    Auth is still messed up, so lets comment this out now, and work on it in a separate branch like we should have done from the start.

--- LSSDDistanceEdReg/LSSDDistanceEdReg.WebFrontEnd/Startup.cs
+++ LSSDDistanceEdReg/LSSDDistanceEdReg.WebFrontEnd/Startup.cs
@@ -31,6 +31,7 @@ namespace LSSDDistanceEdReg.WebFrontEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            /*
             services.AddAuthentication(options =>
             {
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
@@ -57,10 +58,10 @@ namespace LSSDDistanceEdReg.WebFrontEnd
                 };
             });
             services.AddAuthorization(); 
-            
-            services.AddRazorPages().AddRazorPagesOptions(options => {
+            */
+            services.AddRazorPages();/*.AddRazorPagesOptions(options => {
                 options.Conventions.AuthorizeFolder("/Admin");
-            });
+            }); */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
