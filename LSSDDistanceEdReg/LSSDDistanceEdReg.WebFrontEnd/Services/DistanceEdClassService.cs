06c0997f14b64ce8b4a372c42a722e2c490925ba
Mark Strendin <mark.strendin@lskysd.ca>
2020-08-19 15:27:04 -0600

    Actually include the services

---
+++ LSSDDistanceEdReg/LSSDDistanceEdReg.WebFrontEnd/Services/DistanceEdClassService.cs
@@ -0,0 +1,39 @@
+﻿using LSSD.DistanceEdReg;
+using LSSD.DistanceEdReg.Data;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSSDDistanceEdReg.WebFrontEnd.Services
+{
+    public class DistanceEdClassService
+    {
+        private IConfiguration _configuration { get; set; }
+        private DistanceEdClassRepository _classRepo;
+
+        public DistanceEdClassService(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            this._classRepo = new DistanceEdClassRepository(_configuration.GetConnectionString(FrontendSettings.ConnectionStringName));
+        }
+
+
+        public List<DistanceEdClass> GetAll()
+        {
+            return _classRepo.GetAllClasses();
+        }
+
+        public List<DistanceEdClass> GetAllAvailable(DateTime effectiveDate)
+        {
+            return _classRepo.GetAvailableClasses(effectiveDate);
+        }
+
+        public DistanceEdClass Get(int ID)
+        {
+            return _classRepo.Get(ID);
+        }
+
+    }
+}
---
+++ LSSDDistanceEdReg/LSSDDistanceEdReg.WebFrontEnd/Services/DistanceEdRequestService.cs
@@ -0,0 +1,53 @@
+﻿using LSSD.DistanceEdReg;
+using LSSD.DistanceEdReg.Data;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSSDDistanceEdReg.WebFrontEnd.Services
+{
+    public class DistanceEdRequestService
+    {
+        private IConfiguration _configuration { get; set; }
+        private DistanceEdRequestRepository _repository { get; set; }
+
+        public DistanceEdRequestService(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            this._repository = new DistanceEdRequestRepository(_configuration.GetConnectionString(FrontendSettings.ConnectionStringName));
+        }
+
+        public List<DistanceEdRequest> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public List<DistanceEdRequest> GetForCourse(DistanceEdClass Course)
+        {
+            return _repository.GetForCourse(Course);
+        }
+
+        public List<DistanceEdRequest> GetForCourse(int Course)
+        {
+            return _repository.GetForCourse(Course);
+        }
+
+        public List<DistanceEdRequest> GetForCourse(string Course)
+        {
+            return _repository.GetForCourse(Course);
+        }
+
+        public void Add(DistanceEdRequest NewRequest)
+        {
+            _repository.AddNewRequest(NewRequest);
+        }
+
+        public void Add(List<DistanceEdRequest> NewRequests)
+        {
+            _repository.AddNewRequests(NewRequests);
+        }
+
+    }
+}
